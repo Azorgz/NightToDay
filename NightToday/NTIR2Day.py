e74c38a5e16b8b207e1c94bfcc8c418427ae213d
test <test@test.fr>
2026-01-30 15:44:02 +0100

    Update of TL sample merging

--- NightToday/NTIR2Day.py
+++ NightToday/NTIR2Day.py
@@ -213,7 +213,9 @@ class Image2ImageGAT_Dual(nn.Module):
                 if checkpoint is None:
                     assert isinstance(epoch, (str, int)), "When loading full checkpoints, epoch must be str or int."
                     save_filename = f'{epoch}_net_{self.model_name}'
-                    save_path = os.path.join(self.opt.training.checkpoint_dir, save_filename)
+                    path = os.getcwd() + '/NightToday/CrossRAFT/checkpoints/' if 'laptop' in socket.gethostname() else \
+                        '/bettik/PROJECTS/pr-remote-sensing-1a/godeta/checkpoints/NightToday/'
+                    save_path = os.path.join(path, save_filename)
                     checkpoint = torch.load(save_path, weights_only=False, map_location='cpu')
                     if return_checkpoint:
                         return checkpoint
