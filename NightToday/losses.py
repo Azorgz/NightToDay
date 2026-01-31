5edafe3dfefc5bd35c3b8d682b71b35e5b7d4678
test <test@test.fr>
2026-01-31 08:14:33 +0100

    Update of TL sample merging

--- NightToday/losses.py
+++ NightToday/losses.py
@@ -197,9 +197,9 @@ class ColorConsistencyLoss(nn.Module):
         super(ColorConsistencyLoss, self).__init__()
         self.lambda_saturation = 0.5
         self.lambda_lab_edge = 0.05
-        self.lambda_l1 = 0.025
+        self.lambda_l1 = 0.05
         self.lambda_contrast = 5.
-        self.saturation_gated = 0.25
+        self.saturation_gated = 0.05
 
     def forward(self, fake_color: torch.Tensor, real_color: torch.Tensor, mask_high_color) -> torch.Tensor:
         """
@@ -361,7 +361,7 @@ def ColorLoss(fake_color, real_color, GT_seg=None, th_high=0.95, th_low=0.15, we
         #                    (fake_sky_region + 1 - (GT_seg == SKY).float()).mean(dim=1).flatten(1).min(dim=1)[0]) * 0.2
     else:
         loss += (color_dist * high_color_mask).sum(dim=[1, 2, 3]) / (high_color_mask.sum(dim=[1, 2, 3]) + 1e-6)
-    loss += ColorConsistencyLoss()(im_fake.to_tensor(), im_target.to_tensor(), high_color_mask) * 0.2
+    loss += ColorConsistencyLoss()(im_fake.to_tensor(), im_target.to_tensor(), valid) * 0.2
 
     return loss.mean()
 
--- download_res.sh
+++ download_res.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+#REMOTE_PATH="godeta@cargo.univ-grenoble-alpes.fr:/bettik/PROJECTS/pr-remote-sensing-1a/godeta/checkpoints/NightToday/latest_net_ResNet"
+#LOCAL_PATH="/home/godeta/PycharmProjects/MyTransform/checkpoints/download/"
 REMOTE_PATH="godeta@cargo.univ-grenoble-alpes.fr:/bettik/PROJECTS/pr-remote-sensing-1a/godeta/training_visuals/"
-#LOCAL_PATH="$HOME/Images/result-bigfoot/image/"
 LOCAL_PATH="/home/godeta/PycharmProjects/MyTransform/training_visuals/"
 
 rsync -avxH -c "$REMOTE_PATH" "$LOCAL_PATH"
