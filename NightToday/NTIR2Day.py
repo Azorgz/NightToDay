9d3f1dd529868f36718b7595c83b4b67648e8492
test <test@test.fr>
2026-02-20 11:18:50 +0100

    Update of the FLIR dataset loading

--- NightToday/NTIR2Day.py
+++ NightToday/NTIR2Day.py
@@ -612,13 +612,14 @@ class Image2ImageGAT_Dual(nn.Module):
                                                        self.segMask_TN_update, weights=self.class_weight)
         self.loss_contour[self.T] += self.compute_loss('contour', self.fake_D, self.segMask_TN_update)
 
-        encoded_TD, _, _, real_D = self.netG.encode(self.real_D_T, self.real_D, from_=self.T, epoch=self.epoch)
-        encoded_D = self.netG.encode(real_D, from_=self.D)
-        self.fake_D_day = self.netG.decode(encoded_TD, to_=self.D)
-        self.loss_color_day[self.T] += self.compute_loss('latent', encoded_TD, encoded_D, loss_name='color_day',
-                                                         criterion_lambda='color_day')
-        self.loss_color_day[self.D] += self.compute_loss('cycle', self.fake_D_day, real_D, loss_name='color_day',
-                                                         criterion_lambda='color_day')
+        if self.real_D_T is not None:
+            encoded_TD, _, _, real_D = self.netG.encode(self.real_D_T, self.real_D, from_=self.T, epoch=self.epoch)
+            encoded_D = self.netG.encode(real_D, from_=self.D)
+            self.fake_D_day = self.netG.decode(encoded_TD, to_=self.D)
+            self.loss_color_day[self.T] += self.compute_loss('latent', encoded_TD, encoded_D, loss_name='color_day',
+                                                             criterion_lambda='color_day')
+            self.loss_color_day[self.D] += self.compute_loss('cycle', self.fake_D_day, real_D, loss_name='color_day',
+                                                             criterion_lambda='color_day')
         # endregion
 
         # combined loss
