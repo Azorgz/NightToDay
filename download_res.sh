ff98c17f997eb47cc19300178552cdc7e9f4824a
Azorgz <aurel.godet@gmail.com>
2026-02-26 09:07:03 +0100

    Update of TL sample merging

--- download_res.sh
+++ download_res.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #REMOTE_PATH="godeta@cargo.univ-grenoble-alpes.fr:/bettik/PROJECTS/pr-remote-sensing-1a/godeta/checkpoints/NightToday/"
-#LOCAL_PATH="/home/godeta/PycharmProjects/MyTransform/checkpoints/download/"
+#LOCAL_PATH="/home/aurelien/PycharmProjects/NightToDay/checkpoints/download/"
 #rsync -avxH -c "$REMOTE_PATH" "$LOCAL_PATH"
 
 REMOTE_PATH="godeta@cargo.univ-grenoble-alpes.fr:/silenus/PROJECTS/pr-remote-sensing-1a/godeta/training_visuals/"
-LOCAL_PATH="/home/godeta/PycharmProjects/MyTransform/training_visuals/"
+LOCAL_PATH="/home/aurelien/PycharmProjects/NightToDay/training_visuals/"
 rsync -avxH -c "$REMOTE_PATH" "$LOCAL_PATH"
