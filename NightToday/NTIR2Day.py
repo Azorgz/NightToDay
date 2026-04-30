8a180bf26dab041cc5a72f8b827144ddc43e6f62
test <test@test.fr>
2026-04-30 16:18:02 +0200

    Saving pattern update

--- NightToday/NTIR2Day.py
+++ NightToday/NTIR2Day.py
@@ -563,7 +563,8 @@ class NightToDay(nn.Module):
                                                        self.segMask_TN_update, weights=self.class_weight)
         self.loss_thermal[self.T] += self.compute_loss('thermal', self.remapped_T, self.real_T, self.real_T,
                                                        self.segMask_TN_update, weights=self.class_weight)
-        self.loss_contour[self.T] += self.compute_loss('contour', self.fake_D, self.segMask_TN_update)
+        self.loss_contour[self.T] += self.compute_loss('contour', self.fake_TN, self.segMask_TN_update)
+        self.loss_contour[self.D] += self.compute_loss('contour', self.fake_T, self.segMask_D_update)
         self.loss_sky[self.D] += self.compute_loss('sky', self.fake_D, self.segMask_TN_update)
 
         if self.real_D_T is not None and self.lambda_color_day > 0.0:
