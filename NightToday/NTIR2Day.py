a67add2762227689863cc59b62c2f4172eb11616
test <test@test.fr>
2026-04-30 21:19:48 +0200

    Saving pattern update

--- NightToday/NTIR2Day.py
+++ NightToday/NTIR2Day.py
@@ -460,8 +460,8 @@ class NightToDay(nn.Module):
         # endregion
 
         # region Fusion Loss
-        self.loss_sharpness[self.N] += self.compute_loss('sharpness', self.fake_TN, self.real_N, self.real_T)
-        self.loss_sharpness[self.T] += self.compute_loss('sharpness', self.fake_TN, self.real_T, self.real_T)
+        self.loss_sharpness[self.N] += self.compute_loss('sharpness', self.fake_TN, self.real_N, self.remapped_T)
+        self.loss_sharpness[self.T] += self.compute_loss('sharpness', self.remapped_T, self.real_T, self.real_T)
         gray_N = .299 * self.real_N[:, 0:1, :, :] + .587 * self.real_N[:, 1:2, :, :] + .114 * self.real_N[:, 2:3, :, :]
         self.loss_fus[self.N] += self.compute_loss('cycle', self.fake_TN, -gray_N.repeat(1, 3, 1, 1),
                                                    loss_name='fus', criterion_lambda='fus')
