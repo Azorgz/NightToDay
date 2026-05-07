a254a7b9990e6aeaa693c82d27b8951a22022d72
test <test@test.fr>
2026-05-07 14:21:58 +0200

    few updates

--- NightToday/losses.py
+++ NightToday/losses.py
@@ -758,7 +758,7 @@ class ContrastiveLoss(nn.Module):
         local_mean = split_images.view(B, C, N, -1).mean(dim=-1)
         local_tv = split_gradients.view(B, C, N, -1).mean(dim=-1) * 0.2
         loss = local_tv / (local_std + 1e-6) * (local_mean * (1 - local_mean) + 1e-6) * 0.1
-        return loss.mean() + torch.abs(image - gt).mean() * 0.1 - dy.abs().mean(dim=[2, 3]) * 0.1
+        return loss.mean() + torch.abs(image - gt).mean() * 0.1 - dy.abs().mean() * 0.1
 
 
 class ThermalNoiseLoss(nn.Module):
