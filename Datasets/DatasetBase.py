ed64c20e392e7a9ac74fb92a1b0369683a8a472f
test <test@test.fr>
2026-05-13 11:04:56 +0200

    few updates

--- Datasets/DatasetBase.py
+++ Datasets/DatasetBase.py
@@ -76,7 +76,7 @@ class TrainDataset(Dataset):
         self.num_classes = opt.num_classes
         random_size = (opt.load_size[0] * (torch.rand(1).item() * 0.6 + 0.4)) // 8 * 8
         self.augmentations = Augmentations([hflip if opt.augmentations['hflip'] else None,
-                                            RandomCrop((random_size, random_size)) if opt.augmentations['random_crop'] else None])
+                                            RandomCrop((random_size, random_size)) if opt.augmentations['crop'] else None])
         self.load_size = opt.load_size
         self.resize_and_crop = opt.resize_and_crop
         opt.sampling = opt.sampling if opt.sampling > 0 else 1
