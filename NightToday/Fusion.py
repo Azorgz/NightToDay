0fdf7057b3023704edcbdee877820d76d69e49d4
test <test@test.fr>
2026-06-01 10:40:21 +0200

    few updates

--- NightToday/Fusion.py
+++ NightToday/Fusion.py
@@ -441,6 +441,8 @@ class FastIRDenoiser(nn.Module):
         try:
             ROOT_DIR = Path(__file__).resolve().parent.parent / 'checkpoints/' if 'laptop' in socket.gethostname() else \
                 Path(f'/bettik/PROJECTS/${PROJECT_NAME}/godeta/checkpoints/NightToday')
+            path = ROOT_DIR / 'fast_ir_denoiser.pth'
+            print(path)
             state_dict = torch.load(ROOT_DIR / 'fast_ir_denoiser.pth', map_location='cpu')
             self.load_state_dict(state_dict, strict=False)
         except FileNotFoundError:
